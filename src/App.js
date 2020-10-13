1c67ffc1b041c715cbaabddc6fced58a613e6fd2
anickel101 <anson.nickel@gmail.com>
2020-10-13 14:53:02 -0400

    delete task working

--- src/App.js
+++ src/App.js
@@ -43,12 +43,22 @@ class App extends React.Component {
     this.setState({selected: category})
   }
 
+  //no real identifier available without shenanigans, so this will delete duplicates (just like the demo...)
+  deleteTask = (deletedTask) => {
+    console.log(deletedTask.text)
+    let newTaskList = [...this.state.tasks]
+    let taskIndex = newTaskList.findIndex(task => task.text === deletedTask.text)
+    console.log(taskIndex)
+    newTaskList.splice(taskIndex, 1)
+    this.setState({tasks: newTaskList})
+  }
+
 
   render() {
     return (
       <div className="App">
         <h2>My tasks</h2>
-        <TasksContainer categories={CATEGORIES} selected={this.state.selected} changeCategory={this.changeCategory} tasks={this.state.tasks}/>
+        <TasksContainer categories={CATEGORIES} selected={this.state.selected} changeCategory={this.changeCategory} tasks={this.state.tasks} deleteTask={this.deleteTask}/>
       </div>
     );
   }
--- src/Containers/TasksContainer.js
+++ src/Containers/TasksContainer.js
@@ -6,7 +6,7 @@ class TasksContainer extends React.Component {
 
     renderTasks = () => {
         if (this.props.selected === "All") {
-            return this.props.tasks.map((task,index) => <Task task={task} key={index} />)
+            return this.props.tasks.map((task,index) => <Task task={task} key={index} deleteTask={this.props.deleteTask}/>)
         } else {
             return this.props.tasks.filter(task => task.category === this.props.selected).map((task,index) => <Task task={task} key={index} />)
         }
